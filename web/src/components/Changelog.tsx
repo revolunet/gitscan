04ef633e4be9484388aeefc2b5031fd8f1075fed
Julien Bouquillon <julien@bouquillon.com>
2026-02-07 00:24:51 +0100

    wip

--- web/src/components/Changelog.tsx
+++ web/src/components/Changelog.tsx
@@ -2,6 +2,19 @@
 
 import Markdown from "react-markdown";
 
+const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
+function prefixPath(path: string | undefined): string | undefined {
+  if (
+    typeof path === "string" &&
+    path.startsWith("/") &&
+    !path.startsWith("//")
+  ) {
+    return `${basePath}${path}`;
+  }
+  return path;
+}
+
 interface ChangelogProps {
   content: string;
 }
@@ -9,7 +22,24 @@ interface ChangelogProps {
 export function Changelog({ content }: ChangelogProps) {
   return (
     <div className="prose prose-slate prose-sm max-w-none">
-      <Markdown>{content}</Markdown>
+      <Markdown
+        components={{
+          a: ({ href, children, ...props }) => (
+            <a {...props} href={prefixPath(href)}>
+              {children}
+            </a>
+          ),
+          img: ({ src, alt, ...props }) => (
+            <img
+              src={prefixPath(typeof src === "string" ? src : undefined)}
+              alt={alt}
+              {...props}
+            />
+          ),
+        }}
+      >
+        {content}
+      </Markdown>
     </div>
   );
 }
